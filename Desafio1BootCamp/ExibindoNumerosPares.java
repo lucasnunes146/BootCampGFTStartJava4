948cc6d5bb616dab5e98ccaea8d7df48b27d0b26
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-02-17 15:10:50 -0300

    URI1059 (Modificado)

---
+++ Desafio1BootCamp/ExibindoNumerosPares.java
@@ -0,0 +1,21 @@
+package Desafio1BootCamp;
+
+import java.util.Scanner;
+
+public class ExibindoNumerosPares {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int valor = entrada.nextInt();
+        int aux = 0;
+
+        for (int i = 0; i <= valor; i ++){
+            if(aux % 2 == 0 && aux != 0){
+                System.out.println(aux);
+                
+            }  
+            aux++;
+        }
+        entrada.close();
+    }
+}
