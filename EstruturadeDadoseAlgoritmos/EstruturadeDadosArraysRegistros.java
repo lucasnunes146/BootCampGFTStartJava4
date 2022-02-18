61b2628745fc18838a627c49a3b5d7f27cc4146c
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-02-17 23:04:29 -0300

    Estrutura de Dados

---
+++ EstruturadeDadoseAlgoritmos/EstruturadeDadosArraysRegistros.java
@@ -0,0 +1,17 @@
+// 
+
+//vetores e matrizes: auxiliam quando existem várias variáveis de mesmo tipo;
+
+//vetores: armazenam determinada quantidade de variáveis do mesmo tipo;
+
+public class EstruturadeDadosArraysRegistros{
+    public static void main(String[] args) {
+        int vetor[] = {30, 45, 54, 55};
+
+        for (int posicao = 0; posicao <= 3; posicao++){
+            System.out.println(vetor[posicao]+ " ");
+        }
+    }
+}
+
+
